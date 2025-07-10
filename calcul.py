6a8de824660540cb17a1f9adb19120033bce3290
mar-ion8 <marion.bleuse8@gmail.com>
2025-07-10 17:46:20 +0200

    premier commit = création du fichier de connexion à l'interface dédiée et à l'interface principale

---
+++ calcul.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Import module PyQt et API PyQGIS
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+from qgis.core import *
+from qgis.core import Qgis, QgsMessageLog
+from qgis.utils import iface
+from qgis import processing
+import os
+
+#import librairie nécessaire au requêtage SQL
+import sqlite3
+
+# import librairie lecture CSV
+import pandas as pd
+
+from datetime import datetime, timedelta
+
+# appel emplacement des fichiers de stockage des sorties temporaires -- temp
+temp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "temp")
+
+# lien entre traitement.py et traitement.ui
+ui_path = os.path.dirname(os.path.abspath(__file__))
+ui_path = os.path.join(ui_path, "ui")
+form_traitement, _ = uic.loadUiType(os.path.join(ui_path, "calcul.ui"))
+
+
+# mise en place de la classe TraitementWidget
+# va regrouper l'ensemble des fonctions relatives aux traitements à réaliser
+class CalculWidget(QDialog, form_traitement):
+    def __init__(self, iface):
+        QDialog.__init__(self)
+
+        # création de l'interface de la fenêtre QGIS
+        self.setupUi(self)
+        # ajustement de la taille de la fenêtre pour qu'elle soit fixe
+        #self.setFixedSize(600, 400)
+        # nom donné à la fenêtre
+        self.setWindowTitle("Top'Eau - Analyse des données eau : écoute biodiversité")
+
+        # Bouton "OK / Annuler"
+        self.terminer.rejected.connect(self.reject)
+
+        # connexion de la barre de progression
+        self.progressBar.setValue(0)
